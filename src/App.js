76d5789052fc30c79d04e2426c3e0952c3c6b054
Niksed1 <image22061@gmail.com>
2025-02-04 16:38:45 -0800

    status

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
+import Status from './components/Status'
 
 function App() {
   return (
     <main>
       <Header/>
+      <Status/>
     </main>
   );
 }
---
+++ src/components/Status.js
@@ -0,0 +1,9 @@
+export default function Status() {
+
+    return (
+        <div className="Status">
+            <h2>You win!</h2>
+            <p>Well done! 🎉</p>
+        </div>
+    );
+}
